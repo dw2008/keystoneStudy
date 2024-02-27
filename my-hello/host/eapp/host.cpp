78071477c852319a1345919db053285d3c782bf9
dw2008 <tiantianwoah@gmail.com>
2024-02-26 22:46:57 -0800

    Update my-hello

--- my-hello/host/eapp/host.cpp
+++ my-hello/host/eapp/host.cpp
@@ -9,19 +9,19 @@ using namespace Keystone;
 
 int
 main(int argc, char** argv) {
-  Enclave enclave;
-  Params params;
+    Enclave enclave;
+    Params params;
 
-  params.setFreeMemSize(256 * 1024);
-  params.setUntrustedMem(DEFAULT_UNTRUSTED_PTR, 256 * 1024);
+    params.setFreeMemSize(3 * 1024 * 1024);
+    params.setUntrustedSize(256 * 1024);
 
-  enclave.init(argv[1], argv[2], argv[3], params);
+    enclave.init(argv[1], argv[2], argv[3], params);
 
-  enclave.registerOcallDispatch(incoming_call_dispatch);
-  edge_call_init_internals(
-      (uintptr_t)enclave.getSharedBuffer(), enclave.getSharedBufferSize());
+    enclave.registerOcallDispatch(incoming_call_dispatch);
+    edge_call_init_internals(
+            (uintptr_t)enclave.getSharedBuffer(), enclave.getSharedBufferSize());
 
-  enclave.run();
+    enclave.run();
 
-  return 0;
+    return 0;
 }
